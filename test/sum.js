eca2328b43f3eb83c3f1a9a073bd8cb9c75ea16d
Ho Ho <ho.ho@nogle.com>
2017-11-07 11:21:39 +0800

    Fix: sum is not a function

--- test/sum.js
+++ test/sum.js
@@ -4,14 +4,14 @@ var sum = require('../lib/sum');
 
 describe('#sum', function(){
     it('should return the sum of an array', function(done){
-        var sum = sum([1, 2, 3, 4]);
-        sum.should.equal(10);    // 測試有沒有取得正確的最小值
+        var total = sum([1, 2, 3, 4]);
+        total.should.equal(10);    // 測試有沒有取得正確的最小值
         done();
     })
 
     it('should return undefined when array is empty', function(done){
-        var sum = sum([]);
-        (sum === undefined).should.be.true; // 測試有沒有傳回 undefined
+        var total = sum([]);
+        (total === undefined).should.be.true; // 測試有沒有傳回 undefined
         done();
     })
 })
