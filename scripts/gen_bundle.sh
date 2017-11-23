1309ae2d5cbcfd467641f0b9d7a0f0db06e6d29c
Ho Ho <ho.ho@nogle.com>
2017-11-23 11:06:24 +0800

    Modify .travis.yml

--- scripts/gen_bundle.sh
+++ scripts/gen_bundle.sh
@@ -1,45 +1,29 @@
 
 # github info
-branch="test"
+branch="bundle"
 repo="https://github.com/hoho1234578/Front.git"
 date=`date +%Y-%m-%d`
-commit_log="generate bundle for testing on ${date}"
+commit_log="Generate bundle for testing on ${date}"
 
-# rm -rf build
-# cd build
+rm -rf build
+git clone $repo --depth 1 -b $branch build # Depth 用於指定 clone 深度，為 1 表示只 clone 最近一次 commit
+rm -rf build/*
 
-git clone --depth=1 $repo --branch $branch build # Depth 用於指定 clone 深度，為 1 表示只 clone 最近一次 commit
+# Move files in dist folder to build folder
+yarn build
+cp -r dist/. build/
 
-# exists=`git rev-parse --verify origin/test`
-# echo $?
+# Add
+cd build
+git add . -f
 
-# if [$? -eq 0]
-# then
-# 	echo "Branch exists"
+# Commit
+echo $commit_log
+git commit -m "$commit_log"
 
-# # else
+# Push
+git push origin $branch -f
 
-# fi
-
-# mkdir -p build # Creat build folder if not exist
-# rm -rf build/*
-# git clone --depth=1 $repo --branch $branch build # Depth 用於指定 clone 深度，為 1 表示只 clone 最近一次 commit
-
-# Copy the latest files under dist folder to build folder
-# rm -rf build/*
-# cp -r dist/. build/
-
-# # Add
-# cd build
-# git add . -f
-
-# # Commit
-# echo $commit_log
-# git commit -m "$commit_log"
-
-# # Push
-# git push origin $branch -f
-
-# # Remove build folder
-# cd ..
-# rm -rf build
+# Remove build folder
+cd ..
+rm -rf build
